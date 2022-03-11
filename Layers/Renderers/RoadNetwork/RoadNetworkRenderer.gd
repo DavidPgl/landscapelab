09334934af3c1b4ffbd62099955fe4eda4587fb0
DavidPgl <panagiotopulos.d@gmail.com>
2022-03-11 15:31:28 +0100

    Added better road subdivision function

--- Layers/Renderers/RoadNetwork/RoadNetworkRenderer.gd
+++ Layers/Renderers/RoadNetwork/RoadNetworkRenderer.gd
@@ -27,17 +27,18 @@ const heightmap_sample_rate: int = heightmap_size / heightmap_resolution
 
 
 # OVERRIDE #
+# Runs in a thread!
 func load_new_data():
 	var road_network_info: Layer.RoadNetworkRenderInfo = layer.render_info
 	var road_features = road_network_info.road_layer.get_features_near_position(center[0], center[1], radius, max_features)
 	var intersection_features = road_network_info.intersection_layer.get_features_near_position(center[0], center[1], radius, max_features)
-	
+
 	for road in road_features:
 		_create_road(road, road_network_info.road_instance_scene)
 
 
 # OVERRIDE #
-func apply_new_data():	
+func apply_new_data():
 	# Remove old objects
 	for child in get_children():
 		child.queue_free()
@@ -50,7 +51,7 @@ func apply_new_data():
 	for intersection in intersections:
 		add_child(intersection)
 
-# Runs in a thread!
+
 func _create_road(road_feature, road_instance_scene: PackedScene) -> void:
 	var road_id = road_feature.get_attribute("edge_id")
 	var road_width = float(road_feature.get_attribute("width"))
@@ -66,87 +67,86 @@ func _create_road(road_feature, road_instance_scene: PackedScene) -> void:
 		point = Vector3(point.x, _get_height_at_ground(point), point.z)
 		road_curve.set_point_position(index, point)
 	
-	# TODO: Lower resolution causes addtional points to be outside of curve
+	
 	# Add additional curve points depending on heightmap resolution
 	var current_point_index: int = 0
-	var next_point_index: int = 1
-	var curve_point_count: int = road_curve.get_point_count()
-	for curve_point_index in range(curve_point_count):
-		# Actual curve points
-		var current_point = road_curve.get_point_position(current_point_index)
-		var next_point = road_curve.get_point_position(next_point_index)
-		# Direction of curve edge
-		var direction = next_point - current_point
-		# Additional curve points on x axis
-		var points_to_add_on_x = []
-		# Number of intersections with the sample grid on x
-		var number_of_x_intersects: int = ceil(abs(direction.x) / heightmap_sample_rate)
-		for i in range(number_of_x_intersects):
-			var x
-			
-			# If the direction is positive, go to the next grid on the right
-			if sign(direction.x) == 1:
-				x = (heightmap_sample_rate * (i + 1) - fmod(abs(current_point.x), heightmap_sample_rate))
-			# If the direction is negative, go to the next grid on the left
-			else:
-				x = (fmod(abs(current_point.x), heightmap_sample_rate) + heightmap_sample_rate * i) * -1 
-			
-			# Calculate z with the z,x ratio of the direction
-			var z = direction.z / direction.x * x
-			
-			# Add values as offsets to original point
-			var point = Vector3(current_point.x + x, 0, current_point.z + z)
-			# Get height for additional point
-			point = Vector3(point.x, _get_height_at_ground(point), point.z)
-			points_to_add_on_x.append(point)
-		
-		# Additional curve points on x axis
-		var points_to_add_on_z = []
-		# Number of intersections with the sample grid on z
-		var number_of_z_intersects: int = ceil(abs(direction.z) / heightmap_sample_rate)
-		for i in range(number_of_z_intersects):
-			var z
-			
-			# If the direction is positive, go to the next grid on the right
-			if sign(direction.z) == 1:
-				z = (heightmap_sample_rate * (i + 1) - fmod(abs(current_point.z), heightmap_sample_rate))
-			# If the direction is negative, go to the next grid on the left
-			else:
-				z = (fmod(abs(current_point.z), heightmap_sample_rate) + heightmap_sample_rate * i) * -1 
-			
-			# Calculate x with the x,z ratio of the direction
-			var x = direction.x / direction.z * z
-			
-			# Add values as offsets to original point
-			var point = Vector3(current_point.x + x, 0, current_point.z + z)
-			# Get height for additional point
-			point = Vector3(point.x, _get_height_at_ground(point), point.z)
-			points_to_add_on_z.append(point)
-		
-		var x_index = 0
-		var z_index = 0
-		for i in range(points_to_add_on_x.size() + points_to_add_on_z.size()):
-			current_point_index += 1
-			if x_index >= points_to_add_on_x.size():
-				road_curve.add_point(points_to_add_on_z[z_index], Vector3(), Vector3(), current_point_index)
-				z_index += 1
-				continue
-			if z_index >= points_to_add_on_z.size():
-				road_curve.add_point(points_to_add_on_x[x_index], Vector3(), Vector3(), current_point_index)
-				x_index += 1
-				continue
-			
-			if current_point.distance_squared_to(points_to_add_on_x[x_index]) < current_point.distance_squared_to(points_to_add_on_z[z_index]):
-				road_curve.add_point(points_to_add_on_x[x_index], Vector3(), Vector3(), current_point_index)
-				x_index += 1
-			else:
-				road_curve.add_point(points_to_add_on_z[z_index], Vector3(), Vector3(), current_point_index)
-				z_index += 1
-		
-		
-		
-		next_point_index = current_point_index + 1
+	var curve_point_count = road_curve.get_point_count()
 	
+	# Go through each road edge
+	for i in range(curve_point_count - 1):
+		var current_point = road_curve.get_point_position(current_point_index)
+		var next_point = road_curve.get_point_position(current_point_index + 1)
+	
+		var x_grid_point = null
+		var z_grid_point = null
+		
+		var next_point_index = current_point_index + 1
+		while current_point_index < next_point_index:
+			# Direction of curve edge
+			var direction = next_point - current_point
+			
+			# Only calculate grid point if we don't have one and there actually is one
+			if x_grid_point == null && abs((next_point.x / heightmap_sample_rate) - (current_point.x / heightmap_sample_rate)) >= 1:
+				
+				# Calculate x sampling grid offset
+				var x_offset
+
+				# If the direction is positive, go to the next grid on the right
+				if sign(direction.x) == 1:
+					x_offset = (heightmap_sample_rate - fmod(abs(current_point.x), heightmap_sample_rate))
+				# If the direction is negative, go to the next grid on the left
+				else:
+					x_offset = (fmod(abs(current_point.x), heightmap_sample_rate)) * -1 
+				
+				# Going from one x grid point to the next
+				if x_offset == 0:
+					x_offset += heightmap_sample_rate * sign(direction.x)
+				
+				var z = direction.z / direction.x * x_offset
+				x_grid_point = Vector3(current_point.x + x_offset, 0, current_point.z + z)
+				x_grid_point = Vector3(x_grid_point.x, _get_height_at_ground(x_grid_point), x_grid_point.z)
+			
+			# Only calculate grid point if we don't have one and there actually is one
+			if z_grid_point == null && abs((next_point.z / heightmap_sample_rate) - (current_point.z / heightmap_sample_rate)) >= 1:
+				
+				# Calculate sampling grid z offset
+				var z_offset
+
+				# If the direction is positive, go to the next grid on the right
+				if sign(direction.z) == 1:
+					z_offset = (heightmap_sample_rate - fmod(abs(current_point.z), heightmap_sample_rate))
+				# If the direction is negative, go to the next grid on the left
+				else:
+					z_offset = (fmod(abs(current_point.z), heightmap_sample_rate)) * -1
+				
+				# Going from one z grid point to the next
+				if z_offset == 0:
+					z_offset += heightmap_sample_rate * sign(direction.z)
+				
+				var x = direction.x / direction.z * z_offset
+				z_grid_point = Vector3(current_point.x + x, 0, current_point.z + z_offset)
+				z_grid_point = Vector3(z_grid_point.x, _get_height_at_ground(z_grid_point), z_grid_point.z)
+			
+			# If no grid points, done with this curve edge
+			if x_grid_point == null && z_grid_point == null:
+				break
+			
+			# Add closest one
+			if z_grid_point == null || x_grid_point != null && current_point.distance_squared_to(x_grid_point) <= current_point.distance_squared_to(z_grid_point):
+				road_curve.add_point(x_grid_point, Vector3(), Vector3(), current_point_index + 1)
+				current_point = x_grid_point
+				x_grid_point = null
+			else:
+				road_curve.add_point(z_grid_point, Vector3(), Vector3(), current_point_index + 1)
+				current_point = z_grid_point
+				z_grid_point = null
+			
+			# Move to newly added point and start from there again
+			current_point_index += 1
+			next_point_index += 1
+		
+		# Required to start from correct next point (while loop stops before)
+		current_point_index += 1
 	
 	# TODO: Handle terrain LOD change
 	
